80ac167cc0a7808340b27e71787dd9b026e86f2a
ingeboPuzen <oftedal.ingeborg@gmail.com>
2014-02-20 14:39:31 +0100

    Changed fram support fragment to regular fragment

--- UbiLearnProject/src/no/ntnu/stud/ubilearn/Training.java
+++ UbiLearnProject/src/no/ntnu/stud/ubilearn/Training.java
@@ -1,7 +1,7 @@
 package no.ntnu.stud.ubilearn;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +15,7 @@ public class Training extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		
-
 	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle b){
