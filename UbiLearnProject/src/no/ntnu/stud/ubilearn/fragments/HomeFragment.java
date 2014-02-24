47b04942ebf9c76190ceafb21a5679db58365d79
espehel <espenhellerud5@gmail.com>
2014-02-24 09:49:05 +0100

    refactoring

--- UbiLearnProject/src/no/ntnu/stud/ubilearn/fragments/HomeFragment.java
+++ UbiLearnProject/src/no/ntnu/stud/ubilearn/fragments/HomeFragment.java
@@ -13,6 +13,6 @@ public class HomeFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 					Bundle savedInstanceState)
 	{
-		return inflater.inflate(R.layout.fragment_test_home, container, false);
+		return inflater.inflate(R.layout.fragment_home, container, false);
 	}
 }
