3e711d95fe743e4f8a78aeb67c5cc2c7c8d95afd
kyrrelm <kyrre.l.moe@gmail.com>
2014-02-25 18:07:42 +0100

    trying to remove scrollview

--- UbiLearnProject/src/no/ntnu/stud/ubilearn/fragments/WikiFragment.java
+++ UbiLearnProject/src/no/ntnu/stud/ubilearn/fragments/WikiFragment.java
@@ -10,21 +10,25 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class WikiFragment extends Fragment {
-
-	private RelativeLayout rl;
 	private View root;
+	private ListView categoryListView;
+	private String[] categories;
 	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle b){
-		root = inflater.inflate(R.layout.fragment_wiki_fragment, null);
-		//rl = (RelativeLayout) root.findViewById(R.id.training_rel);
+		categories= getResources().getStringArray(R.array.menu_options);
 		
+		root = inflater.inflate(R.layout.fragment_wiki_fragment, null);
+		categoryListView = (ListView) root.findViewById(R.id.wikiListView);
+		categoryListView.setAdapter(new ArrayAdapter<String>(this.getActivity(), R.layout.drawer_list_item, categories));
 		
 		return root;
 	}
-
 }
