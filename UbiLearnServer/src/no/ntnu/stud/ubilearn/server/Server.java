f9a236d1404e2a1f9830cc074f1b08aaf11f9fe0
Kyrre <kyrre.l.moe@gmail.com>
2014-03-10 12:21:49 +0100

    created a server project using maven

--- UbiLearnServer/src/no/ntnu/stud/ubilearn/server/Server.java
+++
@@ -1,5 +0,0 @@
-package no.ntnu.stud.ubilearn.server;
-
-public class Server {
-
-}
---
+++ ubilearn-server/src/main/java/no/ntnu/stud/ubilearn/ubilearn_server/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.ubilearn.ubilearn_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
