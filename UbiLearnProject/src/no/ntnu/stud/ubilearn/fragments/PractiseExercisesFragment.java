09a0ed8c561a1785188f352dcf5406e6a8d89d59
Vegar Lerpoll <vegarlerpoll@gmail.com>
2014-03-10 15:23:05 +0100

    Practise-classes

---
+++ UbiLearnProject/src/no/ntnu/stud/ubilearn/fragments/PractiseExercisesFragment.java
@@ -0,0 +1,18 @@
+package no.ntnu.stud.ubilearn.fragments;
+
+import no.ntnu.stud.ubilearn.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PractiseExercisesFragment extends Fragment 
+{
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.fragment_practise_exercises, container, false);
+		
+		return view;
+	}
+}
