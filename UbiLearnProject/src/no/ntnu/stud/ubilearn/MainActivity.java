ddd36f17f95d3dcacb12ba29d020823a0bf3a293
espehel <espenhellerud5@gmail.com>
2014-04-04 02:00:06 +0200

    small changes

--- UbiLearnProject/src/no/ntnu/stud/ubilearn/MainActivity.java
+++ UbiLearnProject/src/no/ntnu/stud/ubilearn/MainActivity.java
@@ -227,27 +227,34 @@ public class MainActivity extends Activity {
 	
 	private void logout() {
 		
-		clearBackstack();
-		ParseUser.logOut();
-		Intent intent = new Intent(this, LoginActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	    startActivity(intent);
+//		clearBackstack();
+//		ParseUser.logOut();
+//		Intent intent = new Intent(this, LoginActivity.class);
+//		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//	    startActivity(intent);
 		
 		//-----------------------------DATABASE TESTING--------------------------------------
 		
-//		HandbookDAO hb = new HandbookDAO(this);
-//		hb.open();
-//		hb.insertCategory(new Category("abc123", "Tests", new Date(), null));
-//		hb.insertCategory(new Category("asd123", "subTests", new Date(), "abc123"));
-//		hb.insertArticle(new Article("qwe123", "testArtikkel", "detter er en test bla bla bla, massse kult innhold jippiii", new Date(), "asd123"));
+		HandbookDAO hb = new HandbookDAO(this);
+		hb.open();
+		hb.insertCategory(new Category("abc123", "Tests", new Date(), null));
+		hb.insertCategory(new Category("asd123", "subTests", new Date(), "abc123"));
+		hb.insertArticle(new Article("qwe123", "testArtikkel", "detter er en test bla bla bla, massse kult innhold jippiii", new Date(), "asd123"));
 
-////		Log.d("MAIN",hb.getCategory("abc123").printContent());
+//		Log.d("MAIN",hb.getCategory("abc123").printContent());
 		
-//		List<WikiItem> content = hb.getHandbook();
-//		for (WikiItem item : content) {
-//			Log.d("SUPER",item.printContent());
-//		}
-//		hb.close();
+		List<WikiItem> content = hb.getHandbook();
+		System.out.println(content.size());
+		for (WikiItem item : content) {
+			Log.d("SUPER",item.printContent());
+			if(item instanceof Category){
+				for (WikiItem sub : ((Category)item).getSub()) {
+					Log.d("SUB",sub.printContent());
+
+				}
+			}
+		}
+		hb.close();
 		
 //		TrainingDAO tDAO = new TrainingDAO(this);
 //		tDAO.open();
